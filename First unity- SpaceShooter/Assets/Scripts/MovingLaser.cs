c3d57c43e8e0cb1c4c19941a173b27581cef7783
zTigers <36008743+zTigers@users.noreply.github.com>
2018-02-14 14:22:07 -0500

    [ADD] SpaceShip boundary + Laser Fire
    added spaceship boundaries and together with mouse  button firing lasers.

---
+++ First unity- SpaceShooter/Assets/Scripts/MovingLaser.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingLaser : MonoBehaviour {
+    public float speed;
+    private Rigidbody2D rBody;
+    public Boundary boundary;
+
+
+
+	// Use this for initialization
+	void Start () {
+        rBody = this.GetComponent<Rigidbody2D>();
+        rBody.velocity = this.transform.right * speed; // Set velocity = (1,0)
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- First unity- SpaceShooter/Assets/Scripts/PlayerController.cs
+++ First unity- SpaceShooter/Assets/Scripts/PlayerController.cs
@@ -2,17 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Boundary class
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, yMin, yMax;
+}
 public class PlayerController : MonoBehaviour {
 
     public float speed;
+    public float nextFire = 0.25f;
+    public Boundary boundary;
+    public GameObject Laser;
+    public Transform LaserSpawn;
+    /*
+    public float xMin;
+    public float yMin;
+    public float xMax;
+    public float yMax;
+    */
 
-	// Use this for initialization
-	void Start () {
-		
+    //Private Variables
+    private Rigidbody2D rBody;
+    private float myTime = 0.0f;
+
+    // Use this for initialization
+    void Start () {
+        rBody = this.GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        myTime += Time.deltaTime;
+
+        if(Input.GetButton("Fire1") && myTime > nextFire)
+        {
+            Instantiate(Laser, LaserSpawn.position, LaserSpawn.rotation);
+
+            myTime = 0.0f;
+        }
 		
 	}
     //used when preforming physics calculations
@@ -25,9 +53,13 @@ public class PlayerController : MonoBehaviour {
 
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
 
-        Rigidbody2D rBody = this.gameObject.GetComponent<Rigidbody2D>(); // this = whole structure of program. gameObject = menu. GetCompontent<> = specific in menu. This line establish connection of movement to Rigidbody2D
+        //Rigidbody2D rBody = this.gameObject.GetComponent<Rigidbody2D>(); // this = whole structure of program. gameObject = menu. GetCompontent<> = specific in menu. This line establish connection of movement to Rigidbody2D
         rBody.velocity = movement * speed;
-        
-        
+
+        //rBody.position = new Vector2(Mathf.Clamp(rBody.position.x, -8.14f, 3.5f), Mathf.Clamp(rBody.position.y, -4.19f, 4.19f));
+        //rBody.position = new Vector2(Mathf.Clamp(rBody.position.x, xMin, xMax), Mathf.Clamp(rBody.position.y, yMin, yMax));
+        rBody.position = new Vector2(Mathf.Clamp(rBody.position.x, boundary.xMin, boundary.xMax), Mathf.Clamp(rBody.position.y, boundary.yMin, boundary.yMax));
+
+
     }
 }
