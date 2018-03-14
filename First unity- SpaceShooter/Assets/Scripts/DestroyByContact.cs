b760c8d0b45983c71fc2ab0fffebd55387ca14b0
zTigers <36008743+zTigers@users.noreply.github.com>
2018-03-14 14:24:00 -0400

    [ADD] Asteroid SpawnRate & SpawnWait. score & gameover
    Added spawn waves of asteroids and log of score + gameover

--- First unity- SpaceShooter/Assets/Scripts/DestroyByContact.cs
+++ First unity- SpaceShooter/Assets/Scripts/DestroyByContact.cs
@@ -5,10 +5,23 @@ using UnityEngine;
 public class DestroyByContact : MonoBehaviour {
 
     public GameObject explosionAsteroid;
+    public GameObject PlayerExplosion;
+
+    private GameController gameControllerScript;
+    public int scoreValue = 10;
 
 	// Use this for initialization
 	void Start () {
-		
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+
+        if(gameControllerObject != null)
+        {
+            gameControllerScript = gameControllerObject.GetComponent<GameController>();
+        }
+        if(gameControllerScript == null)
+        {
+            Debug.Log("Cannot find game controller script on object");
+        }
 	}
 	
 	// Update is called once per frame
@@ -19,8 +32,22 @@ public class DestroyByContact : MonoBehaviour {
             return;
             //Debug.Log("DestroyByContact");
         }
+        if (other.tag == "Player")
+        {
+            Vector3 deltaP = (this.transform.position + other.transform.position) / 2; // finds half way point between two objects
 
-        Instantiate(explosionAsteroid, this.transform.position, this.transform.rotation);
+            // Create our explosion animation
+            Instantiate(PlayerExplosion, deltaP, other.transform.rotation);
+
+            //GameOver!
+            gameControllerScript.GameOver();
+        }
+        else
+        {
+            Instantiate(explosionAsteroid, this.transform.position, this.transform.rotation);
+            gameControllerScript.AddScore(scoreValue);
+        }
+
 
         Destroy(other.gameObject); // destroy other game object
         Destroy(this.gameObject); // Destroying thus thing (the asteroid 
---
+++ First unity- SpaceShooter/Assets/Scripts/GameController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject hazard; // what are we spawning
+    public Vector2 spawnValue; // where do we spawn Hazards
+    public int hazardCount; // how many hazards per wave
+    public float startWait; // how long until hazard wave starts
+    public float spawnWait; // how long between each hazard in each wave
+    public float waveWait; // how long between each wave of enemies
+
+    private bool gameOver;
+    private bool restart;
+    private int score;
+
+	// Use this for initialization
+	void Start () {
+        score = 0;
+
+        StartCoroutine(SpawnWaves());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait); // pause Wait time before game first wave
+        while(true)
+        {
+            for(int i = 0; i < hazardCount; i++)
+            {
+                Vector2 spawnPosition = new Vector2(spawnValue.x, Random.Range(-spawnValue.y,spawnValue.y));
+                //                                      12                         -3.5          3.5
+
+                Quaternion spawnRotation = Quaternion.identity; // default rotation
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait); //Wait time between spawning each asteroid
+            }
+            yield return new WaitForSeconds(waveWait);
+
+            if(gameOver)
+            {
+                break;
+            }
+        }
+    }
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        // score = score + newScoreValue
+        Debug.Log("Score is " + score);
+    }
+
+    public void GameOver()
+    {
+        Debug.Log("Game is Over");
+        gameOver = true;
+    }
+}
